07b28856c66962069d944c7f0dbef227a87326d1
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-18 18:48:02 -0300

    Estrutura inicial projeto vite

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <main>
+      <small>Olá, me chamo</small>
+      <h1 className="text-4xl">Deyvid</h1>
+      <h2>Desenvolvedor Web</h2>
+      <div>
+        <button>Baixar CV 
+          <img src="/download.svg" alt="Ícone de download" />
+        </button>
+        <button>E-mail
+          <img src="/email.svg" alt="Ícone de e-mail" />
+        </button>
+      </div>
+    </main>
+  )
+}
+
+export default App;
