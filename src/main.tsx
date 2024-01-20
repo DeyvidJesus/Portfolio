9ad9cc746663b5196a06a6cd51970c47aa55bfe3
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-20 14:07:32 -0300

    Theme Provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/global.css';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from './styles/muiStyle.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
