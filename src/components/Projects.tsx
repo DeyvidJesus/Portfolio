7f9886ce5058f43f9c093a2385f1746ebc61311c
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-01-22 13:08:00 -0300

    Projects section

---
+++ src/components/Projects.tsx
@@ -0,0 +1,121 @@
+export function Projects() {
+  return (
+    <section id="Projects" className="sm:w-full md:w-10/12 flex flex-col items-center">
+      <h2 className="font-serif font-semibold my-6 text-3xl border-b-2 border-royal-blue text-center">Experiências</h2>
+      <ul>
+        <li className="flex flex-col items-center mb-16">
+          <div className="flex justify-center bg-white w-5/6 rounded-lg shadow-xl">
+            <div className="font-serif font-medium p-7 w-3/5">
+              <span className="flex items-center mb-2">
+                <h3 className="text-xl font-sansSerif">Nossas Mulheres</h3>
+                <img className="w-8 ml-2" src="/laco.svg" alt="" />
+              </span>
+              <p className="text-justify">Durante o curso técnico, integrei um time de 8 alunos em um projeto para <strong>agilizar a identificação e tratamento do câncer de mama</strong>. Minha contribuição central foi desenvolver a <strong>estrutura e validação dos formulários</strong>, assegurando a <strong>inserção precisa dos dados no banco</strong>. Essa experiência não apenas fortaleceu minha capacidade técnica, mas também ressaltou a importância da <strong>colaboração em equipe</strong> no desenvolvimento web para <strong>otimização de processos de saúde</strong>.</p>
+              <p className="text-royal-blue flex my-4">O site foi retirado do ar, após o fim da campanha!</p>
+              <ul className="flex justify-around">
+                <li className="flex flex-col items-center">
+                  <img className="w-12" src="/htmlIcon.svg" alt="" />
+                  HTML5
+                </li>
+                <li className="flex flex-col items-center">
+                  <img className="w-12" src="/cssIcon.svg" alt="" />
+                  CSS3
+                </li>
+                <li className="flex flex-col items-center">
+                  <img className="w-12" src="/phpIcon.svg" alt="" />
+                  PHP
+                </li>
+                <li className="flex flex-col items-center">
+                  <img className="w-12" src="/mysqlIcon.svg" alt="" />
+                  MySQL
+                </li>
+                <li className="flex flex-col items-center">
+                  <img className="w-12" src="/gitIcon.svg" alt="" />
+                  Git
+                </li>
+              </ul>
+            </div>
+            <div className="w-2/5 bg-[url('nossasMulheres.svg')] bg-cover border-l-2 rounded">
+            </div>
+          </div>
+        </li>
+        <li className="flex flex-col items-center mb-16">
+          <div className="flex justify-center bg-white w-5/6 rounded-lg shadow-xl">
+            <div className="font-serif font-medium p-7 w-3/5">
+              <span className="flex items-center mb-2">
+                <h3 className="text-xl font-sansSerif">All.do</h3>
+                <img className="w-8 ml-2" src="/alldoIcon.svg" alt="" />
+              </span>
+              <p className="text-justify">Com a capacidade de <strong>criar projetos</strong>, <strong>adicionar tarefas</strong> e personalizar a experiência com <strong>modo claro/escuro</strong>, o all.do visa ajudar na organização diária.
+                <br />
+                Apesar de ser um projeto individual, integrei <strong>MongoDB</strong> para armazenamento e utilizei <strong>integrações com Github e Facebook para autenticação</strong>. Enfrentei desafios ao aprender novas tecnologias, configurar logins sociais e fazer deploy do aplicativo, resultando em valiosas lições sobre padronização e configuração eficiente.</p>
+              <span className="flex">
+                <a href="https://all-do.vercel.app" target="_blank" className="text-royal-blue flex my-4 mr-6">Site no ar!</a>
+                <a href="https://github.com/DeyvidJesus/all.do" target="_blank" className="text-royal-blue flex my-4">Repositório do projeto</a>
+              </span>
+              <ul className="flex justify-around">
+                <li className="flex flex-col items-center">
+                  <img className="w-12" src="/nextIcon.svg" alt="" />
+                  NextJS
+                </li>
+                <li className="flex flex-col items-center">
+                  <img className="w-12" src="/typescriptIcon.svg" alt="" />
+                  Typescript
+                </li>
+                <li className="flex flex-col items-center">
+                  <img className="w-12" src="/mongodbIcon.svg" alt="" />
+                  MongoDB
+                </li>
+                <li className="flex flex-col items-center">
+                  <img className="w-12" src="/tailwindIcon.svg" alt="" />
+                  Tailwind
+                </li>
+                <li className="flex flex-col items-center">
+                  <img className="w-12" src="/gitIcon.svg" alt="" />
+                  Git
+                </li>
+              </ul>
+            </div>
+            <div className="w-2/5 bg-[url('alldo.svg')] bg-cover border-l-2 rounded"></div>
+          </div>
+        </li>
+        <li className="flex flex-col items-center mb-16">
+          <div className="flex justify-center bg-white w-5/6 rounded-lg shadow-xl">
+            <div className="font-serif font-medium p-7 w-3/5">
+              <span className="flex items-center mb-2">
+                <h3 className="text-xl font-sansSerif">Consulado Fitness</h3>
+                <img className="w-8 ml-2" src="/laco.svg" alt="" />
+              </span>
+              <p className="text-justify">Durante o curso técnico, integrei um time de 8 alunos em um projeto para <strong>agilizar a identificação e tratamento do câncer de mama</strong>. Minha contribuição central foi desenvolver a <strong>estrutura e validação dos formulários</strong>, assegurando a <strong>inserção precisa dos dados no banco</strong>. Essa experiência não apenas fortaleceu minha capacidade técnica, mas também ressaltou a importância da <strong>colaboração em equipe</strong> no desenvolvimento web para <strong>otimização de processos de saúde</strong>.</p>
+              <p className="text-royal-blue flex my-4">O site foi retirado do ar, após o fim da campanha!</p>
+              <ul className="flex justify-around">
+                <li className="flex flex-col items-center">
+                  <img className="w-12" src="/htmlIcon.svg" alt="" />
+                  HTML5
+                </li>
+                <li className="flex flex-col items-center">
+                  <img className="w-12" src="/cssIcon.svg" alt="" />
+                  CSS3
+                </li>
+                <li className="flex flex-col items-center">
+                  <img className="w-12" src="/phpIcon.svg" alt="" />
+                  PHP
+                </li>
+                <li className="flex flex-col items-center">
+                  <img className="w-12" src="/mysqlIcon.svg" alt="" />
+                  MySQL
+                </li>
+                <li className="flex flex-col items-center">
+                  <img className="w-12" src="/gitIcon.svg" alt="" />
+                  Git
+                </li>
+              </ul>
+            </div>
+            <div className="w-2/5 bg-[url('nossasMulheres.svg')] bg-cover border-l-2 rounded">
+            </div>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
+}
