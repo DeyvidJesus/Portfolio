4161eba1b6a726c0fc2183228c6ef8e063c79b18
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2024-02-06 10:26:59 -0300

    Adiciona o curriculo para download no botão de CV

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,7 +4,7 @@ export function Main() {
       <h1 className="sm:text-xl lg:text-2xl font-serif text-dark-gray font-medium text-center">Olá, eu sou o <strong className="sm:text-3xl md:text-4xl font-sansSerif">Deyvid!</strong></h1>
       <h2 className="font-serif font-semibold text-royal-blue text-xl sm:my-2">Desenvolvedor Web</h2>
       <div className="flex sm:flex-col lg:flex-row justify-around lg:w-1/4 items-center sm:h-48 lg:h-24">
-        <a download={""} className="bg-royal-blue lg:h-14 flex sm:p-2 lg:p-3 rounded text-white font-semibold items-center justify-between sm:w-44 lg:w-36 cursor-pointer">Baixar CV
+        <a download={"/resume.pdf"} className="bg-royal-blue lg:h-14 flex sm:p-2 lg:p-3 rounded text-white font-semibold items-center justify-between sm:w-44 lg:w-36 cursor-pointer">Baixar CV
           <img src="/downloadIcon.svg" alt="Ícone de download" className="sm:w-6 sm:h-6" />
         </a>
         <a href="mailto:deyvidgondim@outlook.com" className="bg-white border-2 lg:h-14 border-royal-blue flex sm:p-2 lg:p-3 rounded text-black font-semibold items-center justify-between sm:w-44 lg:w-36">E-mail
